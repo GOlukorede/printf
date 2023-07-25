66f61176013a6540f62461e2bdbcd9e287a54bef
Hezekiahs <111096175+Tychool@users.noreply.github.com>
2023-07-25 22:01:34 +0100

    Update precision.c by Tychool

--- precision.c
+++ precision.c
@@ -1,12 +1,11 @@
-#include "main.h"
+#include "printf.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
+ * get_precision - printing precision
+ * @format: format for printing arg
+ * @i: list/array of arg to print
+ * @list: arg list
+ * Return: precision of the output
  */
 int get_precision(const char *format, int *i, va_list list)
 {
