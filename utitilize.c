17f0d8bf1914cd75276e2a96aa90370f5f20e32d
Hezekiahs <111096175+Tychool@users.noreply.github.com>
2023-07-25 22:55:39 +0100

    Update utitilize.c by tychool

--- utitilize.c
+++ utitilize.c
@@ -1,10 +1,9 @@
-#include "main.h"
+#include "printf.h"
 
 /**
- * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
- *
- * Return: 1 if c is printable, 0 otherwise
+ * is_printable - check if an item is printable
+ * @c: input
+ * Return: if printable 1, else 0
  */
 int is_printable(char c)
 {
@@ -15,11 +14,11 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
- * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
- * Return: Always 3
+ * append_hexa_code - add ASCII (hexa format) to buffer
+ * @buffer: character arrays
+ * @i: array index
+ * @ascii_code: ASCII
+ * Return: 3, anytime
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
@@ -38,10 +37,9 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
- *
- * Return: 1 if c is a digit, 0 otherwise
+ * is_digit - confirm if input is digit
+ * @c: input
+ * Return: if c is a digit return 1. else, 0
  */
 int is_digit(char c)
 {
@@ -52,28 +50,10 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
- *
- * Return: Casted value of num
- */
-long int convert_size_number(long int num, int size)
-{
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((short)num);
-
-	return ((int)num);
-}
-
-/**
- * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
- *
- * Return: Casted value of num
+ * convert_size_unsgnd - convert number size in line with specifier
+ * @num: inpute num
+ * @size: size
+ * Return: new sized num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
@@ -83,4 +63,20 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 		return ((unsigned short)num);
 
 	return ((unsigned int)num);
+}
+
+/**
+ * convert_size_number - convert number size in line with specifier
+ * @num: inpute num
+ * @size: size
+ * Return: new sized num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
 }
